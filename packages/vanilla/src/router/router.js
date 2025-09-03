b068af77b96487d5c19c8e5060cd8f191d37c720
nemobim <doag47@gmail.com>
2025-09-04 01:54:54 +0900

    feat: mount 코드들 ssr 환경에서 분기처리

--- packages/vanilla/src/router/router.js
+++ packages/vanilla/src/router/router.js
@@ -1,6 +1,6 @@
 // 글로벌 라우터 인스턴스
-import { Router, ServerRouter } from "../lib";
 import { BASE_URL } from "../constants.js";
+import { Router, ServerRouter } from "../lib";
 
 const CurrentRouter = typeof window !== "undefined" ? Router : ServerRouter;
 
