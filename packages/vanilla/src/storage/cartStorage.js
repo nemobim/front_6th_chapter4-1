81b084ec6e8f7d8f3b26642800178e00439f3111
nemobim <doag47@gmail.com>
2025-09-04 01:18:10 +0900

    fix: createStorage 에서 ssr/csr 처리

--- packages/vanilla/src/storage/cartStorage.js
+++ packages/vanilla/src/storage/cartStorage.js
@@ -1,12 +1,3 @@
 import { createStorage } from "../lib";
 
-const storage =
-  typeof window !== "undefined"
-    ? window.localStorage
-    : {
-        getItem: () => null,
-        setItem: () => {},
-        removeItem: () => {},
-      };
-
-export const cartStorage = createStorage("shopping_cart", storage);
+export const cartStorage = createStorage("shopping_cart");
